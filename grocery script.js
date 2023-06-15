fdb45946bb34b42a35edbdbe97b19e9ab6d09d5e
Avadhoot1234 <123140903+Avadhoot1234@users.noreply.github.com>
2023-06-15 22:32:16 +0530

    Add files via upload

---
+++ grocery script.js
@@ -0,0 +1,84 @@
+let searchForm=document.querySelector('.search-form');
+let shopcart=document.querySelector('.shopping-cart');
+let loginForm=document.querySelector('.login-form');
+let navbar=document.querySelector('.navbar');
+
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    shopcart.classList.remove('active');
+}
+
+document.querySelector('#cart-btn').onclick=()=>{
+    shopcart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+   
+}
+
+document.querySelector('#login-btn').onclick=()=>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');   
+    shopcart.classList.remove('active');
+}
+
+document.querySelector('#menu-btn').onclick=()=>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    shopcart.classList.remove('active');
+}
+
+window.onscroll=()=>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    shopcart.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
